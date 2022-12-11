e7498b724cf7df73a4f9d773c134a6aaa1da36b3
andmansim <91722324+andmansim@users.noreply.github.com>
2022-12-11 21:39:33 +0100

    prueba uno filtrar

--- ejercicio1.py
+++ ejercicio1.py
@@ -77,7 +77,7 @@ print('\nDiagrama de dispersón peso, potencia----------------------------------
 #a = df.groupby('cylinders').sum()
 plt.scatter(x =df.cylinders, y = df.weight/10, color = 'red', marker = '*', label = 'weight/10')#Se divide entre 10 para que estén más juntas las escalas
 plt.scatter(x =df.cylinders, y = df.horsepower, color = 'green', marker = '^', label = 'horsepower')
-plt.title('Diagrama dispersión peso-potencia')
+plt.title('Diagrama dispersión peso-potencia respecto cylinder')
 plt.xlabel('Cylinder')
 plt.ylabel('Weight, Horsepower')
 plt.show()
@@ -89,7 +89,7 @@ plt.show()
 
 plt.scatter(x =df['model year'], y = df.weight/10, color = 'red', marker = '*', label = 'weight/10')#Se divide entre 10 para que estén más juntas las escalas
 plt.scatter(x =df['model year'], y = df.horsepower, color = 'green', marker = '^', label = 'horsepower')
-plt.title('Diagrama dispersión peso-potencia')
+plt.title('Diagrama dispersión peso-potencia respecto model year')
 plt.xlabel('Model year')
 plt.ylabel('Weight, Horsepower')
 plt.show()
@@ -97,6 +97,21 @@ plt.show()
 # en cambio en el segundo está más repartido por los años
 
 #5.1.6 Saca el mismo gráfico que dibujaste en el ejercicio 5.1.5, esta vez solamente para los coches fabricados en el año 70.
+
+for i in range(len(df)):
+    if df['model year'][i] == 70:
+        df['horse_model'][i] = df['horsepower'][i]
+        df['weight_model'][i] = df['weight'][i]
+        
+print(df['horse_model'])
+print(df['weight_model'])
+'''plt.scatter(x ='70', y = df.weight/10, color = 'red', marker = '*', label = 'weight/10')#Se divide entre 10 para que estén más juntas las escalas
+plt.scatter(x ='70', y = df.horsepower, color = 'green', marker = '^', label = 'horsepower')
+plt.title('Diagrama dispersión peso-potencia respecto model year')
+plt.xlabel('Model year')
+plt.ylabel('Weight, Horsepower')
+plt.show()
+'''
 
 #5.1.7 Saca el mismo gráfico que dibujaste en el ejercicio 5.1.5, esta vez solamente para los coches fabricados en el año 80. 
 # ¿Cuáles son las diferencias más destacables entre las relaciones dibujadas del año 70 y del año 80?
